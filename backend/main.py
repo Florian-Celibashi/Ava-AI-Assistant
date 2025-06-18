5f82ac8dcba66226375e2de5cb6390d63b6a0cf9
florian-celibashi <floriantoo@icloud.com>
2025-06-18 04:15:51 -0400

    Minor tweaks for production

--- backend/main.py
+++ backend/main.py
@@ -46,7 +46,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[os.getenv("FRONTEND_URL", "http://localhost:5173")],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
