c3d863212bf23a869512cc8693d584e1edfa4d31
florian-celibashi <floriantoo@icloud.com>
2025-06-18 14:49:00 -0400

    Patched CORS config to allow local and deployed frontend

--- backend/main.py
+++ backend/main.py
@@ -46,7 +46,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.getenv("FRONTEND_URL", "http://localhost:5173")],
+    allow_origins=[
+        os.getenv("FRONTEND_URL", "http://localhost:5173")
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
